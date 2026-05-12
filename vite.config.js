ee9ffd873c4fce45fe44e7f4a99f28606b126f36
archana1969dubey-code <archana1969dubey@gmail.com>
2026-05-12 16:11:39 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist",
-    sourcemap: false,
-    target: "es2018",
-  },
-});
+  server: {
+    allowedHosts: true
+  }
+})
